55aca731f4aca553c44d9ebc48bb66cae24d0d26
gramekcox <graeme.k.cox@gmail.com>
2018-07-28 17:19:55 -0400

    Added function to combine multiple ticker datasets together, then plotting the correlation heatmap

--- nasdaq_grabber.py
+++ nasdaq_grabber.py
@@ -6,6 +6,8 @@ import os
 import pandas as pd
 pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import numpy as np 
 
 			# print('Already have {}'.format(ticker))
 
@@ -18,9 +20,8 @@ def save_sp500_tickers():
 		ticker = row.findAll('td')[0].text #for all table data, grab text
 		ticker = ticker.replace('.','-').strip()
 		tickers.append(ticker)
-	with open("sp500tickers.pickle","wb") as f:
-		pickle.dump(tickers,f)
-	# print(tickers)
+
+
 	tickers.remove('ANDV')
 	tickers.remove('BKNG')
 	tickers.remove('BF-B')
@@ -34,6 +35,11 @@ def save_sp500_tickers():
 	tickers.remove('UAA')
 	tickers.remove('WELL')
 	tickers.remove('BRK-B')
+
+	with open("sp500tickers.pickle","wb") as f:
+		pickle.dump(tickers,f)
+	# print(tickers)
+
 	return tickers
 
 def get_data_from_yahoo(reload_sp500=False):
@@ -48,10 +54,13 @@ def get_data_from_yahoo(reload_sp500=False):
 	start = dt.datetime(2000, 1, 1)
 	# end   = dt.datetime(2016, 1, 1)
 	end   = dt.datetime.now()
+
+	## Save all tickers to a .csv
 	for ticker in tickers:
 		try:
-			print(ticker)
+			# print(ticker)
 			if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+				print(ticker)
 				df = web.DataReader(str(ticker), 'morningstar', start, end)
 				df.to_csv('stock_dfs/{}.csv'.format(ticker))
 			 	# df.reset_index(inplace=True)
@@ -63,5 +72,66 @@ def get_data_from_yahoo(reload_sp500=False):
 		except:
 			print('Cannot obtain data for ' + ticker)
 
+def compile_data():
+	with open("sp500tickers.pickle", "rb") as f:
+		tickers = pickle.load(f)
 
-get_data_from_yahoo(True)
+	main_df = pd.DataFrame()
+	# print(main_df)
+
+	for count, ticker in enumerate(tickers):
+		df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+		df.set_index('Date', inplace=True)
+
+		df.rename(columns={'Close': ticker}, inplace=True)
+		df.drop(['Open', 'High', 'Symbol', 'Low', 'Volume'], 1, inplace=True)
+
+		if main_df.empty:
+			main_df = df
+		else:
+
+			main_df = main_df.join(df, how='outer')
+
+		if count % 10 == 0:
+			print(count) #just see every 10 so we know its working
+
+
+	print(main_df.head())
+	main_df.to_csv('sp500_joined_closes.csv')
+
+def visualize_data():
+	df = pd.read_csv('sp500_joined_closes.csv')
+
+	#create correlation table for all data
+	df_corr = df.corr()
+
+	# print(df_corr.head())
+	data = df_corr.values # gets inner values, no ticker names
+	fig = plt.figure()
+	ax = fig.add_subplot(1,1,1)
+
+	# Create heatmap with colrs from red to green
+	heatmap = ax.pcolor(data, cmap = plt.cm.RdYlGn)
+	fig.colorbar(heatmap)
+	ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+	ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+
+	ax.invert_yaxis()
+	ax.xaxis.tick_top() #moves ticks to top of heatmap
+
+	column_labels = df_corr.columns 
+	row_labels = df_corr.index 
+	ax.set_xticklabels(column_labels)
+	ax.set_yticklabels(row_labels)
+
+	plt.xticks(rotation=90) #rotate ticks so we can easily read them
+	heatmap.set_clim(-1,1) #limit colors
+
+	plt.tight_layout()
+	plt.show()
+
+
+# save_sp500_tickers()
+# get_data_from_yahoo(True)
+# compile_data()
+visualize_data()
